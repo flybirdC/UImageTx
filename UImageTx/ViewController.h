534895c82589d233a7c60d37e1b50227c86e0feb
wangchong3g <1652801988@qq.com>
2019-01-02 18:19:42 +0800

    Initial Commit

---
+++ UImageTx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UImageTx
+//
+//  Created by 胡玲 on 2019/1/2.
+//  Copyright © 2019 flybird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UImageTx/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UImageTx
+//
+//  Created by 胡玲 on 2019/1/2.
+//  Copyright © 2019 flybird. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
