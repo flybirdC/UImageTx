099f59298852783e88ee5ff7ff7d7d676cf3341f
wangchong3g <1652801988@qq.com>
2019-01-17 22:47:24 +0800

    add image details view

---
+++ UImageTx/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  UImageTx
+//
+//  Created by 胡玲 on 2019/1/17.
+//  Copyright © 2019 flybird. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UImageTx/DetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.m
+//  UImageTx
+//
+//  Created by 胡玲 on 2019/1/17.
+//  Copyright © 2019 flybird. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
